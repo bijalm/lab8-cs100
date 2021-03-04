6595f3bbcde690983a5e188ee00946d45e1a3bee
bijalm <33298527+bijalm@users.noreply.github.com>
2021-03-04 00:38:12 -0800

    Update mult_test.hpp

--- tests/mult_test.hpp
+++ tests/mult_test.hpp
@@ -9,7 +9,7 @@ TEST(MultTest, MultEvaluateNonZero) {
     Op* b = new Op(2);
     Mult* test = new Mult(a,b);
     EXPECT_NEAR(test->evaluate(), 20, 0.001);
-
+    delete test;
 }
 
 TEST(MultTest, MultEvaluateDouble) {
@@ -17,6 +17,7 @@ TEST(MultTest, MultEvaluateDouble) {
     Op* b = new Op(2.5);
     Mult* test = new Mult(a,b);
     EXPECT_NEAR(test->evaluate(), 13.75, 0.001);   
+    delete test;
 }
 
 TEST(MultTest, MultEvaluateMultZero) {
@@ -24,6 +25,7 @@ TEST(MultTest, MultEvaluateMultZero) {
     Op* b = new Op(0);
     Mult* test = new Mult(a,b);
     EXPECT_EQ(test->evaluate(), 0);
+    delete test;
 }
 
 TEST(MultTest, MultEvaluateMultNegative) {
@@ -31,6 +33,7 @@ TEST(MultTest, MultEvaluateMultNegative) {
     Op* b = new Op(-10);
     Mult* test = new Mult(a,b);
     EXPECT_NEAR(test->evaluate(), -50, 0.001);
+    delete test;
 }
 
 TEST(MultTest, MultEvaluateMultTwoNegative) {
@@ -38,6 +41,7 @@ TEST(MultTest, MultEvaluateMultTwoNegative) {
     Op* b = new Op(-10);
     Mult* test = new Mult(a,b);
     EXPECT_NEAR(test->evaluate(), 50, 0.001);
+    delete test;
 }
 
 TEST(MultTest, MultStringifyNonZero) {
@@ -45,6 +49,7 @@ TEST(MultTest, MultStringifyNonZero) {
     Op* b = new Op(10);
     Mult* test = new Mult(a,b);
     EXPECT_EQ(test->stringify(), "(5.000000 * 10.000000)");
+    delete test;
 }
 
 TEST(MultTest, MultStringifyDouble) {
@@ -52,6 +57,7 @@ TEST(MultTest, MultStringifyDouble) {
     Op* b = new Op(10.123);
     Mult* test = new Mult(a,b);
     EXPECT_EQ(test->stringify(), "(5.234000 * 10.123000)");
+    delete test;
 }
 
 TEST(MultTest, MultStringifyNegatives) {
@@ -59,6 +65,7 @@ TEST(MultTest, MultStringifyNegatives) {
     Op* b = new Op(-10);
     Mult* test = new Mult(a,b);
     EXPECT_EQ(test->stringify(), "(-5.000000 * -10.000000)");
+    delete test;
 }
 
 
